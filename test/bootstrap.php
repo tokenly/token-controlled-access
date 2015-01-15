6cd41034fe39dd70d4fc7fad9e968f2b93f8e753
cryptonaut <nrathman@ironcladtech.ca>
2015-01-15 12:43:54 -0800

    added unit test

---
+++ test/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+define('BASE_PATH', dirname(__DIR__));
+define('TEST_PATH', __DIR__);
+require BASE_PATH.'/vendor/autoload.php';
---
+++ test/tests/TestAccess.php
@@ -0,0 +1,17 @@
+<?php
+use Tokenly\TCA\Access;
+use \PHPUnit_Framework_Assert as PHPUnit;
+
+class TestAccess extends PHPUnit_Framework_TestCase
+{
+	public function testTokenAccess()
+	{
+		$tca = new Access;
+		
+		$address = '15fx1Gqe4KodZvyzN6VUSkEmhCssrM1yD7';
+		$conditions = array(array('asset' => 'LTBCOIN', 'amount' => 50000, 'op' => '>', 'stackOp' => 'AND'));
+		
+		$this->assertEquals(true, $tca->checkAccess($address, $conditions);
+	}
+	
+}
